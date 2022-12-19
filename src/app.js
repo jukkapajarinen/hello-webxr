2918eaec495eadb06bc02c3d4bace7b6162cd33a
Jukka Pajarinen <jukka.pajarinen@lumitum.com>
2022-12-20 00:19:46 +0200

    Add family names in AR to controller positions.

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import { ARButton } from 'three/examples/jsm/webxr/ARButton.js';
+import { createText } from 'three/examples/jsm/webxr/Text2D.js';
 
 let camera, scene, renderer;
 let controller;
@@ -22,17 +23,13 @@ function init() {
   container.appendChild( renderer.domElement );
   document.body.appendChild( ARButton.createButton( renderer ) );
   const geometry = new THREE.CylinderGeometry( 0, 0.05, 0.2, 32 ).rotateX( Math.PI / 2 );
-
-  function onSelect() {
-    const material = new THREE.MeshPhongMaterial( { color: 0xffffff * Math.random() } );
-    const mesh = new THREE.Mesh( geometry, material );
-    mesh.position.set( 0, 0, - 0.3 ).applyMatrix4( controller.matrixWorld );
-    mesh.quaternion.setFromRotationMatrix( controller.matrixWorld );
-    scene.add( mesh );
-  }
-
   controller = renderer.xr.getController( 0 );
-  controller.addEventListener( 'select', onSelect );
+  controller.addEventListener( 'select', () => {
+    const text = createText(["Jukka", "Kati", "Aapo"][Math.floor((Math.random() * 3))], 0.1 );
+    text.position.set( 0, 0, 0).applyMatrix4( controller.matrixWorld );
+    text.quaternion.setFromRotationMatrix( controller.matrixWorld );
+    scene.add( text );
+  } );
   scene.add( controller );
   window.addEventListener( 'resize', onWindowResize );
 }
